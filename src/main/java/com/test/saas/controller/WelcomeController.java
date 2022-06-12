8a1c9b1fbf53fcad34d5fa54a85d1f1a91156c93
textSolver34761 <pradonbenjamin@gmail.com>
2022-06-12 21:46:03 +0200

    update code working : able to get dummy data

--- src/main/java/com/test/saas/controller/WelcomeController.java
+++ src/main/java/com/test/saas/controller/WelcomeController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.test.saas.model.Welcome;
@@ -17,7 +17,7 @@ public class WelcomeController {
 	@Autowired
 	WelcomeRepository repository;
 	
-	@PostMapping("/")
+	@GetMapping("/")
 	ResponseEntity<List<Welcome>> welcome() {
 		List<Welcome> welcome = repository.findAll();
 	    return new ResponseEntity<List<Welcome>>(welcome, HttpStatus.OK);
